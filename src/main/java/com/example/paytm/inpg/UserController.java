3e856be09c63adfe4b57b2e8b279873c6353232b
coderator143 <codebustershan.619@gmail.com>
2021-01-18 00:07:58 +0530

    Post method implemented and tested in controller class

--- src/main/java/com/example/paytm/inpg/UserController.java
+++ src/main/java/com/example/paytm/inpg/UserController.java
@@ -3,11 +3,11 @@ package com.example.paytm.inpg;
 import com.example.paytm.inpg.entities.User;
 import com.example.paytm.inpg.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
+import java.util.NoSuchElementException;
 // this class is user for handling requests for Restful web services
 @RestController
 public class UserController {
@@ -21,7 +21,18 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}")
-    public User get(@PathVariable Integer id) {
-        return userService.get(id);
+    public ResponseEntity<User> get(@PathVariable Integer id) {
+        try {
+            User user = userService.get(id);
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        }
+        catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/user")
+    public void add(@RequestBody User user) {
+        userService.save(user);
     }
 }
