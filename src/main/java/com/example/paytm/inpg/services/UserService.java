205a9031de5fc1140a3f12bff760dba06e5411c2
coderator143 <codebustershan.619@gmail.com>
2021-01-17 12:23:11 +0530

    Created user controller responsible for handling requests to mysql server

--- src/main/java/com/example/paytm/inpg/services/UserService.java
+++ src/main/java/com/example/paytm/inpg/services/UserService.java
@@ -4,21 +4,19 @@ import com.example.paytm.inpg.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-    public void save(User user) {
-        userRepository.save(user);
-    }
+    public List<User> listAll() { return userRepository.findAll(); }
 
-    public User get(Integer id) {
-        return userRepository.findById(id).get();
-    }
+    public void save(User user) { userRepository.save(user); }
 
-    public void delete(Integer id) {
-        userRepository.deleteById(id);
-    }
+    public User get(Integer id) { return userRepository.findById(id).get(); }
+
+    public void delete(Integer id) { userRepository.deleteById(id); }
 }
