4fb704091d82880fc850a59cd4b0981469c0aa3c
coderator143 <codebustershan.619@gmail.com>
2021-01-21 11:29:12 +0530

    Resolved some edge cases errors of transaction and wallet controller

--- src/main/java/com/example/paytm/inpg/controller/WalletController.java
+++ src/main/java/com/example/paytm/inpg/controller/WalletController.java
@@ -53,7 +53,16 @@ public class WalletController {
             logger.log(Level.INFO, "Mobile number field is empty");
             return new ResponseEntity<>(BAD_REQUEST);
         }
-        User user = userService.findbyMobileNumber(mobileNumber).get(0);
+        List<User> walletUser = userService.findbyMobileNumber(mobileNumber);
+        if(walletUser.isEmpty()) {
+            logger.log(Level.INFO, "User with phone number "+ mobileNumber +" does not exist");
+            return new ResponseEntity<>(NOT_FOUND);
+        }
+        else if(walletUser.size() > 0 && walletUser.get(0).getHaswallet()) {
+            logger.log(Level.INFO, "User already has a wallet registered");
+            return new ResponseEntity<>(BAD_REQUEST);
+        }
+        User user = walletUser.get(0);
         user.setHaswallet(true);
         Wallet wallet = new Wallet();
         wallet.setBalance(0);
@@ -70,12 +79,18 @@ public class WalletController {
                                                 @RequestBody Wallet balanceWallet) {
         List<Wallet> wallets = walletService.findByOwnerID(id);
         if(wallets.isEmpty()) {
-            logger.log(Level.INFO, "Wrong user ID or user hasn't created a wallet");
-            return new ResponseEntity<>(BAD_REQUEST);
+            logger.log(Level.INFO, "User does not exist");
+            return new ResponseEntity<>(NOT_FOUND);
         }
         Wallet wallet = wallets.get(0);
+        int balance = balanceWallet.getBalance();
+        if(balance < 1) {
+            logger.log(Level.INFO, "Cannot add balance <= 0");
+            return new ResponseEntity<>(BAD_REQUEST);
+        }
         wallet.setBalance(wallet.getBalance() + balanceWallet.getBalance());
-        logger.log(Level.INFO, "Balance of "+balanceWallet.getBalance()+" added");
+        logger.log(Level.INFO,
+                "Balance of "+balanceWallet.getBalance()+" added to wallet with id = "+wallet.getId());
         walletService.save(wallet);
         return new ResponseEntity<>(OK);
     }
