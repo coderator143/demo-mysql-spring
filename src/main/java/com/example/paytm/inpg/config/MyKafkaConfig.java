44ae16e9a6b85499412e87acdebd6fa423af3544
coderator143 <codebustershan.619@gmail.com>
2021-01-24 21:25:07 +0530

    Added configurations for kafka consumer to listen to messages

--- src/main/java/com/example/paytm/inpg/config/MyKafkaConfig.java
+++ src/main/java/com/example/paytm/inpg/config/MyKafkaConfig.java
@@ -1,13 +1,15 @@
 package com.example.paytm.inpg.config;
 
 import com.example.paytm.inpg.entities.Transaction;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,5 +30,26 @@ public class MyKafkaConfig {
     @Bean
     public KafkaTemplate<String, Transaction> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ConsumerFactory<String, Transaction> transactionConsumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group-json");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
+                new JsonDeserializer<>(Transaction.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Transaction>
+    concurrentKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Transaction> concurrentKafkaListenerContainerFactory =
+                new ConcurrentKafkaListenerContainerFactory();
+        concurrentKafkaListenerContainerFactory.setConsumerFactory(transactionConsumerFactory());
+        return concurrentKafkaListenerContainerFactory;
     }
 }
